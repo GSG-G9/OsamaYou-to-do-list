562cfa219f5129cebcaf48cc1647f84d891c475a
Osama-you <osama.w.younis@gmail.com>
2020-12-31 11:50:30 +0200

    Create UserLists component and make the stayle and structure for it Relates #8

--- src/components/SideBar/UserLists/index.js
+++ src/components/SideBar/UserLists/index.js
@@ -1,4 +1,14 @@
-const UserLists = () => {
+import './UserLists.css';
 
-};
+const UserLists = () => (
+  <nav className="menu">
+    <a href="!#" className="menuItem">
+      <span>not active</span>
+    </a>
+    <a href="!#" className="menuItem menuItem--active">
+      <span>active</span>
+    </a>
+  </nav>
+);
+
 export default UserLists;
