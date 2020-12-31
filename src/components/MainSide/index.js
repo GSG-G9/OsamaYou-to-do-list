6e4c621412afb23635d2da51e553a5315907da4f
Osama-you <osama.w.younis@gmail.com>
2020-12-31 22:15:01 +0200

    make the haeder and use ToDoList component and pass the props Relates #22

--- src/components/MainSide/index.js
+++ src/components/MainSide/index.js
@@ -1,4 +1,27 @@
-const MainSide = () => {
+import TodoList from "./TodoList";
 
+import "./MainSide.css";
+
+const MainSide = ({ toDoList, onRemoveTask, onAddTask, onCompleteTask }) => {
+  if (!toDoList) {
+    return <main>hi</main>;
+  }
+  return (
+    <main>
+      <header className="main-header">
+        <h1>{toDoList.name}</h1>
+      </header>
+      {onCompleteTask ? (
+        <TodoList
+          tasks={toDoList.tasks}
+          onRemoveTask={onRemoveTask}
+          onAddTask={onAddTask}
+          onCompleteTask={onCompleteTask}
+        />
+      ) : (
+        <h1>no list</h1>
+      )}
+    </main>
+  );
 };
 export default MainSide;
