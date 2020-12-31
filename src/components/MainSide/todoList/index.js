cdd801bd29e57205ba9a6ba0b82812e5cb2db671
Osama-you <osama.w.younis@gmail.com>
2020-12-31 22:09:20 +0200

    do the style and html also display the task list Relates #23

--- src/components/MainSide/todoList/index.js
+++ src/components/MainSide/todoList/index.js
@@ -1,4 +1,36 @@
-const todoList = () => {
+import "./TodoList.css";
 
+const TodoList = ({ tasks, onRemoveTask, onAddTask, onCompleteTask }) => {
+  return (
+    <>
+      <section className="app-insert">
+        <input
+          type="text"
+          name="task"
+          placeholder="Insert your task here..."
+          onKeyDown={onAddTask}
+          autocomplete="off"
+        />
+      </section>
+      <section className="app-list">
+        <ul>
+          {tasks.map((item, i) => (
+            <li
+              key={i}
+              id={i}
+              className={`task${item.isCompleat ? " task-complete" : ""}`}
+              onClick={onCompleteTask}
+            >
+              {item.value}
+              <a href="!#" className="remove-task" onClick={onRemoveTask}>
+                remove
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
 };
-export default todoList;
+
+export default TodoList;
