a2434d820bf19f247783d9638e40f1db5134e2d8
Osama-you <osama.w.younis@gmail.com>
2020-12-31 11:36:21 +0200

    Create UserInfo component and make the stayle and structure for it Relates #7

--- src/components/SideBar/UserInfo/index.js
+++ src/components/SideBar/UserInfo/index.js
@@ -1,4 +1,10 @@
-const UserInfo = () => {
+import './UserInfo.css';
 
-};
+const UserInfo = () => (
+  <div className="avatar">
+    <div className="avatarName">The Void ツ.</div>
+    <input type="text" name="task" placeholder="Insert your list here.." />
+  </div>
+);
+
 export default UserInfo;
